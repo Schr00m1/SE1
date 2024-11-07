5cae8b189b08f2aad7e3c9931fcdf3d7559b6560
Schr00m1 <clemensw2004@gmail.com>
2024-11-07 15:29:40 +0100

    java doc

--- Lesson1/Assigment_1/src/main/java/at/mci/assigment_1/Student.java
+++ Lesson1/Assigment_1/src/main/java/at/mci/assigment_1/Student.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package at.mci.Exercise1.Clemens;
+package at.mci.assigment_1;
 
 import java.util.ArrayList;
 
---
+++ Lesson1/Assigment_1/target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"at.mci.assigment_1","l":"Student"},{"p":"at.mci.assigment_1","l":"TestStudent"}];updateSearchResults();
